04211c57d1a59c97d64db351b2e52d171d94eaa5
sebdok <seb.dok@gmail.com>
2018-07-17 00:23:12 +0200

    dodanie seterow i getwrow w klasie Punkt

--- src/main/java/pl/javastart/obiektowe/Punkt.java
+++ src/main/java/pl/javastart/obiektowe/Punkt.java
@@ -3,4 +3,20 @@ package pl.javastart.obiektowe;
 public class Punkt {
     int wspX;
     int wspy;
+
+    public void ustawX(int x){
+        wspX = x;
+    }
+
+    public void ustawY(int y){
+        wspy = y;
+    }
+
+    public int dajX(){
+        return wspX;
+    }
+
+    public int dajY(){
+        return wspy;
+    }
 }
