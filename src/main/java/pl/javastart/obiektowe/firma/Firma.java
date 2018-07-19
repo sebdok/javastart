52deb40c21b1fc9d9c37ac80cfa7e648c21cab33
sebdok <seb.dok@gmail.com>
2018-07-19 22:32:13 +0200

    dodanie trzech konstruktorow i przetestowanie ich, do wyswietlenia wykorzystano petle 'for each'

--- src/main/java/pl/javastart/obiektowe/firma/Firma.java
+++ src/main/java/pl/javastart/obiektowe/firma/Firma.java
@@ -3,18 +3,22 @@ package pl.javastart.obiektowe.firma;
 public class Firma {
     public static void main(String[] args) {
 
-        Pracownik prac1 =new Pracownik();
-        Pracownik prac2 =new Pracownik();
+       Pracownik[] pracownicy = new Pracownik[3];
 
-        prac1.ustawImie("Adam");
+       pracownicy[0]= new Pracownik("Sebastian","Dokladny", 25);
+       pracownicy[1]= new Pracownik();
+       pracownicy[2]= new Pracownik(pracownicy[0]);
+
+       /* prac1.ustawImie("Adam");
         prac1.ustawNazwisko("Kowalski");
         prac1.ustawWiek(25);
 
         prac2.ustawImie("Jan");
         prac2.ustawNazwisko("Banach");
         prac2.ustawWiek(30);
-
-        System.out.println(prac1);
-        System.out.println(prac2);
+*/
+       for(Pracownik p :pracownicy) {
+           System.out.println(p);
+       }
     }
 }
--- src/main/java/pl/javastart/obiektowe/firma/Pracownik.java
+++ src/main/java/pl/javastart/obiektowe/firma/Pracownik.java
@@ -3,7 +3,7 @@ package pl.javastart.obiektowe.firma;
 public class Pracownik {
     private String imie;
     private String nazwisko;
-    private int wiek;
+    private Integer wiek;
 
     // robimy settery
 
@@ -21,10 +21,29 @@ public class Pracownik {
 
     public int wezWiek(){ return this.wiek; }
 
+    //tworzymy kostruktory
+
+    Pracownik (String imie, String nazwisko, int wiek){
+        this.imie= imie;
+        this.nazwisko= nazwisko;
+        this.wiek= wiek;
+    }
+    Pracownik(){
+        imie = "nie podano";
+        nazwisko = "nie podano";
+        wiek=null;
+        }
+
+    Pracownik(Pracownik pracownik ){
+        imie = pracownik.imie;
+        nazwisko = pracownik.nazwisko;
+        wiek= pracownik.wiek;
+    }
+
     @Override
     public String toString() {
         return
-                imie + '\'' + nazwisko + '\'' + wiek;
+                imie + '\'' + nazwisko + " "+"lat " + wiek;
 
     }
 }
